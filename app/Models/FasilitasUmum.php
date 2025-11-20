9086979797d1e05d523b0a83596f980491162853
kania24si-cyber <kania24si@mahasiswa.pcr.ac.id>
2025-11-20 10:12:30 +0700

    commit dummy data

--- app/Models/FasilitasUmum.php
+++ app/Models/FasilitasUmum.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class FasilitasUmum extends Model
 {
-    protected $table = 'fasilitas_umum';
+    protected $table      = 'fasilitas_umum';
     protected $primaryKey = 'fasilitas_id';
-    public $incrementing = true;
+    public $incrementing  = true;
 
     protected $fillable = [
         'nama',
