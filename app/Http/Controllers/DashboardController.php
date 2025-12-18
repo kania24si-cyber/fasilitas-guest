81cb92c7697c2f760498a75ba0754f8e1aa826af
tiara2sia <kania24si@mahasiswa.pcr.ac.id>
2025-12-18 14:08:28 +0700

    p14perbaikan2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('pages.about.about');
+        return view('pages.about.About');
 
 }
 }
